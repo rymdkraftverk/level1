0a59e76d6f558aaf569410923cba18c6a4662c4c
Simon Lagos <simon.lagos.sallhed@gmail.com>
2017-01-13 14:35:09 +0100

    Add behaviours to entity

--- src/client/entity.js
+++ src/client/entity.js
@@ -1,10 +1,19 @@
 let id = 0;
 
 export function create(){
+  const behaviours = {};
   return {
     id: id++,
-    run: () => {
-      
+    behaviours,
+    run: (entity) => {
+      Object.keys(behaviours).forEach((b) => {
+        const behaviour = behaviours[b];
+        // if (behaviour.init) {
+        //   behaviour.init(behaviour, entity);
+        //   delete behaviour.init;
+        // }
+        behaviour.run(behaviour, entity);
+      });
     }
   }
 }
