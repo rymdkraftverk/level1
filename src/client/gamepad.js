db6b6bdb05935520837b67eb355149724e4fa224
Simon Lagos <simon.lagos.sallhed@gmail.com>
2017-01-19 18:06:05 +0100

    Gamepad v1

---
+++ src/client/gamepad.js
@@ -0,0 +1,45 @@
+const controllers = {};
+let haveEvents = 'ongamepadconnected' in window;
+
+window.addEventListener("gamepadconnected", addgamepad);
+window.addEventListener("gamepaddisconnected", removegamepad);
+
+function removegamepad(gamepad) {
+  delete controllers[gamepad.index];
+}
+
+function addgamepad(gamepad) {
+  controllers[gamepad.index] = gamepad;
+}
+
+export function run(){
+  if (!haveEvents) {
+    scangamepads();
+  }
+}
+
+export function isPressed(id, button){
+  if (!controllers[id]) return;
+
+  const val = controllers[id].buttons[button];
+  let pressed = val == 1.0;
+
+  if (typeof(val) == "object") {
+    pressed = val.pressed;
+    // val = val.value;
+  }
+  return pressed;
+}
+
+function scangamepads() {
+  let gamepads = navigator.getGamepads ? navigator.getGamepads() : (navigator.webkitGetGamepads ? navigator.webkitGetGamepads() : []);
+  for (let i = 0; i < gamepads.length; i++) {
+    if (gamepads[i]) {
+      if (gamepads[i].index in controllers) {
+        controllers[gamepads[i].index] = gamepads[i];
+      } else {
+        addgamepad(gamepads[i]);
+      }
+    }
+  }
+}
