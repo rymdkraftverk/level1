86d264d299f7ac4328eb462413821fef306a0000
Simon Lagos <simon.lagos.sallhed@gmail.com>
2017-01-30 11:40:08 +0100

    Change timer error msg

--- src/client/timer.js
+++ src/client/timer.js
@@ -1,5 +1,5 @@
 export function create(duration){
-  if (!Number.isInteger(duration)) throw new Error('TypeError, Time.create(duration) = duration needs to be integer');
+  if (!Number.isInteger(duration)) throw new Error('TypeError, Timer.create(duration) = duration has to be integer');
   let counter = 0;
 
   return {
