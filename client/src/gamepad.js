8b9052ff424495fbc469b33d4f1b4736d27f089d
Robert Barlin <robert.barlin@jayway.com>
2017-08-29 19:49:22 +0200

    basic models for controller abstraction

--- client/src/gamepad.js
+++ client/src/gamepad.js
@@ -1,5 +1,81 @@
 /* eslint-disable no-undef */
 
+export class L1Analog {
+  constructor(id) {
+    this.id = id;
+    this.flipped = false;
+  }
+
+  invert() {
+    this.flipped = !this.flipped;
+    return this;
+  }
+
+  value(gamepad) {
+    value = gamepad.axes[this.id];
+    return this.flipped ? value * -1 : value;
+  }
+}
+
+export class L1Button {
+  constructor(buttonId) {
+    this.id = buttonId;
+    this.reversed = false;
+  }
+
+  invert() {
+    this.reverse = !this.reverse;
+    return this;
+  }
+
+  isPressed(gamepad) {
+    return this.reversed ? !gamepad.buttons[this.id] : gamepad.buttons[this.id];
+  }
+}
+
+export class L1Controller {
+  constructor(gamepad) {
+    this.id = gamepad.index;
+
+    this.analogs = gamepad.axes.reduce(
+      (acc, a, i) => {
+        acc[i] = new L1Analog(i);
+        return acc;
+      },
+      {},
+    );
+
+    this.analogAliases = Object.keys(this.analogs).reduce(
+      (acc, a, i) => {
+        const axisNbr = Math.floor(i / 2);
+        const name = `axis${axisNbr}${i % 2 === 0 ? 'x' : 'y'}`;
+        acc[name] = i;
+        return acc;
+      },
+      {},
+    );
+
+    this.buttons = gamepad.buttons.reduce(
+      (acc, b, i) => {
+        acc[i] = new L1Button(i);
+        return acc;
+      },
+      {},
+    );
+
+    this.buttonAliases = Object.keys(this.buttons).reduce(
+      (acc, k, i) => {
+        const name = `btn${i}`;
+        acc[name] = i;
+        return acc;
+      },
+      {},
+    );
+  }
+}
+
+const l1Controllers = {};
+
 const controllers = {};
 const haveEvents = 'ongamepadconnected' in window;
 
@@ -8,7 +84,9 @@ function removegamepad(gamepad) {
 }
 
 function addgamepad(gamepad) {
+  l1Controllers[gamepad.index] = new L1Controller(gamepad);
   controllers[gamepad.index] = gamepad;
+  console.log(l1Controllers);
 }
 
 window.addEventListener('gamepadconnected', addgamepad);
