b85e6227c7d9dba4ffed8019cfc1b3bd37916abc
Simon Lagos <simon.lagos.sallhed@gmail.com>
2017-11-26 13:08:15 +0100

    Timers are only run once when they finish

--- client/src/timer.js
+++ client/src/timer.js
@@ -1,16 +1,27 @@
+/* eslint-disable no-else-return */
+
 export function create(duration) {
   if (!Number.isInteger(duration)) throw new Error('TypeError, Timer.create(duration) = duration has to be integer');
   let counter = 0;
-
+  let finished = false;
   return {
     run: () => {
-      if (counter < duration) counter += 1;
-      return (counter === duration);
+      if (finished) {
+        return false;
+      } else if (counter < duration) {
+        counter += 1;
+        return false;
+      } else {
+        finished = true;
+        return true;
+      }
     },
     reset: () => {
       counter = 0;
+      finished = false;
     },
     counter: () => counter,
     duration: () => duration,
+    finished: () => finished,
   };
 }
