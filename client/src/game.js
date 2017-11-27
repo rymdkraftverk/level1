7658d31fa715471561c68aaf98e745c26cc518cc
Simon Lagos <simon.lagos.sallhed@gmail.com>
2017-11-28 00:25:28 +0100

    Enable passing debug as an option to Game.init

--- client/src/game.js
+++ client/src/game.js
@@ -1,20 +1,27 @@
 import * as Core from './core-internal';
 import * as Render from './render-internal';
+import * as Debug from './debug';
 
 const defaultOptions = {
   physics: false,
   // eslint-disable-next-line no-undef 
   element: document.body,
+  debug: false,
 };
 
 export async function init(width, height, sprites, options) {
   // Replace default options with user specified ones 
-  const { physics, element } = { ...defaultOptions, ...options };
+  const {
+    physics,
+    element,
+    debug,
+  } = { ...defaultOptions, ...options };
   console.log('physics', physics);
   console.log('element', element);
   await Render.initRenderer(width, height, sprites, element);
   Core.initMainLoop();
   if (physics) Core.initPhysics();
+  if (debug) Debug.initDebugTools();
 }
 
 export function start() {
