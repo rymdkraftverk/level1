bb0150b9c063ea600d757a1fc369b1f1903645d0
Simon Lagos <simon.lagos.sallhed@gmail.com>
2017-11-28 00:25:07 +0100

    Return text object after it's created

--- client/src/text.js
+++ client/src/text.js
@@ -4,5 +4,7 @@ import { getText, add } from './render-internal';
   Check PIXI.Text docs for available style options
 */
 export function create(text, style = undefined) {
-  add(getText(text, style));
+  const textObject = getText(text, style);
+  add(textObject);
+  return textObject;
 }
