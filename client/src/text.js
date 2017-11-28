e34e5763ea710a0b14edb64e715467a67a0b98ad
Simon Lagos <simon.lagos.sallhed@gmail.com>
2017-11-28 18:46:56 +0100

    Add remove function to Text

--- client/src/text.js
+++ client/src/text.js
@@ -1,4 +1,4 @@
-import { getText, add } from './render-internal';
+import { getText, add, remove as renderRemove } from './render-internal';
 
 /*
   Check PIXI.Text docs for available style options
@@ -7,4 +7,8 @@ export function create(text, style = undefined) {
   const textObject = getText(text, style);
   add(textObject);
   return textObject;
+}
+
+export function remove(textObject) {
+  renderRemove(textObject);
 }
