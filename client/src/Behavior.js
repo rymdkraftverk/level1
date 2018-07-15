50c7b54558011bb88bcd24125d40fe0c34097b98
Simon Lagos <simon.lagos.sallhed@gmail.com>
2018-07-15 15:43:51 +0200

    Add a proposal for Behavior api

--- client/src/Behavior.js
+++ client/src/Behavior.js
@@ -9,3 +9,29 @@
 
 // Behavior.get();
 // Behavior.remove();
+
+// function sort() {
+
+// }
+
+function exists(entity, id) {
+  return entity.behaviors.some((behavior) => behavior.id === id);
+}
+
+export function add(entity, {
+  id, behavior,
+}) {
+  if (exists(entity, id)) {
+    throw new Error(`Behavior with id ${id} already exists on entity ${entity.id}`);
+  }
+  behavior.id = id;
+  entity.behaviors = entity.behaviors.concat(behavior);
+}
+
+export function remove(entity, id) {
+  entity.behaviors = entity.behaviors.filter((behavior) => behavior.id !== id);
+}
+
+export function get(entity, id) {
+  return entity.behaviors.find((behavior) => behavior.id === id);
+}
