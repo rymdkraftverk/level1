acfcdf66f54b11b477c5ed747884c2d00cc8aa2e
Simon Lagos <simon.lagos.sallhed@gmail.com>
2018-08-12 17:29:23 +0200

    Add tests for getX and getY

--- client/test/Entity.js
+++ client/test/Entity.js
@@ -1,5 +1,5 @@
 
-import { getOverlappingArea } from '../src/internal/Entity';
+import { getOverlappingArea, getX, getY } from '../src/internal/Entity';
 
 describe('getOverlappingArea', () => {
   context('non overlapping', () => {
@@ -61,5 +61,33 @@ describe('getOverlappingArea', () => {
       const overlappingArea = getOverlappingArea(entity1, entity2);
       expect(overlappingArea).to.equal(2);
     });
+  });
+});
+
+const grandParent = {
+  x: 20,
+  y: 20,
+  parent: null,
+};
+const parent = {
+  x: 10,
+  y: 10,
+  parent: grandParent,
+};
+const entity = {
+  x: 5,
+  y: 5,
+  parent,
+};
+
+describe('getX', () => {
+  it('returns absolute position', () => {
+    expect(getX(entity)).to.equal(35);
+  });
+});
+
+describe('getY', () => {
+  it('returns absolute position', () => {
+    expect(getY(entity)).to.equal(35);
   });
 });
