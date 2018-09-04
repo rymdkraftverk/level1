372a5c71b2eb80c43880fd27ae11199a94565ff3
Simon Lagos <simon.lagos.sallhed@gmail.com>
2018-09-04 13:45:19 +0200

    WIP: Add new Behavior api proposal

--- client/src/Behavior.js
+++ client/src/Behavior.js
@@ -1,33 +1,70 @@
-// TODO
-
-// Behavior.add(player1, {
-//   id: 'running',
-//   behavior: running,
-//   memoize: true,
-// });
-// // add() enables us to check for duplicate id's when adding
-
-// Behavior.get();
-// Behavior.remove();
-
-// function sort() {
-
-// }
+import uuid from 'uuid/v4';
+import * as Timer from './Timer';
 
 function exists(entity, id) {
   return entity.behaviors.some((behavior) => behavior.id === id);
 }
 
 export function add(entity, {
-  id, behavior,
+  id = uuid(),
+  timer,
+  loop,
+  removeOnComplete,
+  run,
+  onTimerEnd,
+  onInit,
+  onRemove,
+  enabled = true,
+  data,
+  // TODO: Implement
+  // memoize = false,
 }) {
   if (exists(entity, id)) {
     throw new Error(`Behavior with id ${id} already exists on entity ${entity.id}`);
   }
 
-  // TOOD: Throw error if behavior is not an object OR has no run function
-  behavior.id = id;
-  entity.behaviors = entity.behaviors.concat(behavior);
+  const newBehaviorObject = {
+    id,
+    data,
+    timer: Timer.create({ duration: timer }),
+    // eslint-disable-next-line no-shadow
+    init: ({ behavior, entity }) => {
+      if (onInit) {
+        onInit({ behavior, entity });
+      }
+    },
+    // eslint-disable-next-line no-shadow
+    run: ({ behavior, entity }) => {
+      if (!enabled) {
+        return;
+      }
+      if (run) {
+        run({
+          counter: behavior.timer.counter,
+          behavior,
+          entity,
+        });
+      }
+      if (Timer.run(behavior.timer)) {
+        if (onTimerEnd) {
+          onTimerEnd({ behavior, entity });
+        }
+        if (loop) {
+          Timer.reset(behavior.timer);
+        } else if (removeOnComplete) {
+          behavior.remove({ behavior, entity });
+        }
+      }
+    },
+    // eslint-disable-next-line no-shadow
+    remove: ({ behavior, entity }) => {
+      if (onRemove) {
+        onRemove({ behavior, entity });
+      }
+    },
+  };
+
+  entity.behaviors = entity.behaviors.concat(newBehaviorObject);
 }
 
 export function remove(entity, id) {
@@ -36,4 +73,9 @@ export function remove(entity, id) {
 
 export function get(entity, id) {
   return entity.behaviors.find((behavior) => behavior.id === id);
+}
+
+export function reset(entity, id) {
+  const behavior = get(entity, id);
+  Timer.reset(behavior.timer);
 }
