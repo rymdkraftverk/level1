0682c8864eb33c78039dd62e01cd23020f72aee8
Simon Lagos <simon.lagos.sallhed@gmail.com>
2018-09-16 19:10:14 +0200

    Log a warning when trying to delete non-existant behavior

--- client/src/next/entityModifier/removeBehavior.js
+++ client/src/next/entityModifier/removeBehavior.js
@@ -3,6 +3,10 @@ import getBehavior from './getBehavior';
 
 export default curry((id, entity) => {
   const behavior = getBehavior(id, entity);
-  behavior.remove({ entity, data: behavior.data, behavior });
+  if (!behavior) {
+    console.warn(`level1: tried to remove non-existant behavior "${id}" from entity: "${entity.id}"`);
+  } else {
+    behavior.remove({ entity, data: behavior.data, behavior });
+  }
   return entity;
 });
