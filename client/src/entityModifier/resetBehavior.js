9af90c45ad59f6cdc52940411afd181af531719f
Simon Lagos <simon.lagos.sallhed@gmail.com>
2018-10-04 13:44:11 +0200

    Handle behavior not existing when calling resetBehavior

--- client/src/entityModifier/resetBehavior.js
+++ client/src/entityModifier/resetBehavior.js
@@ -3,7 +3,13 @@ import getBehavior from './getBehavior';
 
 export default curry((entity, id) => {
   const behavior = getBehavior(entity, id);
-  behavior.counter = 0;
-  behavior.finished = false;
+
+  if (!behavior) {
+    console.warn(`level1: tried to reset non-existant behavior "${id}" for entity: "${entity.id}"`);
+  } else {
+    behavior.counter = 0;
+    behavior.finished = false;
+  }
+
   return entity;
 });
