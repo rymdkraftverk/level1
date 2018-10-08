3d0c2bdd48886ff879ea300c18dbab43ac4ecc15
Simon Lagos <simon.lagos.sallhed@gmail.com>
2018-10-08 15:41:44 +0200

    Remove currying from l1.scaleText

--- client/src/entityModifier/scaleText.js
+++ client/src/entityModifier/scaleText.js
@@ -1,11 +1,10 @@
-import curry from 'lodash/fp/curry';
 import * as Render from '../internal/Render';
 
 /*
   This is required to be used for any scale change of Text
 */
-export default curry((entity, fontSize) => {
+export default (entity, fontSize) => {
   entity.originalSize = fontSize;
   entity.asset.style.fontSize = fontSize * Render.getRatio();
   return entity;
-});
+};
