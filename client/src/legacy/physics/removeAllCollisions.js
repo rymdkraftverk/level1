d65955387fe44617f5154b8eabffb4462a36c1a2
Simon Lagos <simon.lagos.sallhed@gmail.com>
2018-11-14 14:07:00 +0100

    Remove all physics related code

--- client/src/legacy/physics/removeAllCollisions.js
+++
@@ -1,7 +0,0 @@
-import { Events } from 'matter-js';
-import * as Core from '../internal/Core';
-
-export default () => {
-  const engine = Core.getPhysicsEngine();
-  Events.off(engine);
-};
--- client/src/legacy/physics/removeBody.js
+++
@@ -1,9 +0,0 @@
-import { World } from 'matter-js';
-import getPhysicsEngine from '../physics/getPhysicsEngine';
-
-export default (entity) => {
-  const engine = getPhysicsEngine();
-  World.remove(engine.world, [entity.body]);
-  entity.hasBody = false;
-  entity.body = null;
-};
