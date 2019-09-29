29b4c70dd86de8a20f748f8f4e46a70858be5dd7
Simon Lagos <simon.lagos.sallhed@gmail.com>
2019-09-29 16:55:45 +0900

    Remove keyboard support

--- level1/src/index.js
+++ level1/src/index.js
@@ -453,29 +453,3 @@ export const getOverlappingArea = (displayObject, otherDisplayObject) => {
 
   return dX * dY;
 };
-
-
-// TODO: Deprecate
-/* Keyboard input */
-const pressed = {};
-
-export function isKeyDown(keyCode) {
-  return pressed[keyCode];
-}
-
-function onKeyDown(event) {
-  pressed[event] = true;
-}
-
-function onKeyUp(event) {
-  pressed[event] = false;
-}
-
-export function addKey(key) {
-  Mousetrap.bind(key, () => {
-    onKeyDown(key);
-  }, 'keydown');
-  Mousetrap.bind(key, () => {
-    onKeyUp(key);
-  }, 'keyup');
-}
