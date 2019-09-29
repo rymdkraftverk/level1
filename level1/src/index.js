d9759a59da9765ec1d0a22843a53ddc75f192d72
Simon Lagos <simon.lagos.sallhed@gmail.com>
2019-09-29 17:11:41 +0900

    Remove angle

--- level1/src/index.js
+++ level1/src/index.js
@@ -291,34 +291,22 @@ const onDragEndInternal = (displayObject, onDragEnd, disabler) => () => {
 
 // makeDraggable end
 
-const CLICK_EVENTS = ['click', 'tap']
+const CLICK_EVENTS = ['click', 'tap'];
 
 export const makeClickable = (displayObject, onClick) => {
   // eslint-disable-next-line no-param-reassign
-  displayObject.interactive = true
+  displayObject.interactive = true;
   // eslint-disable-next-line no-param-reassign
-  displayObject.cursor = 'pointer'
+  displayObject.cursor = 'pointer';
 
   CLICK_EVENTS.forEach((event) => {
     displayObject.on(event, () => {
       // eslint-disable-next-line no-param-reassign
-      displayObject.cursor = 'default'
-      onClick()
-    })
-  })
-}
+      displayObject.cursor = 'default';
+      onClick();
+    });
+  });
+};
 
 // TODO: Rename to getGameScale
 export const getScale = () => ratio;
-
-export const angle = ({
-  x1, y1, x2, y2,
-}) => {
-  const xDistance = x2 - x1;
-  const yDistance = y2 - y1;
-  let _angle = Math.atan(yDistance / xDistance);
-  if (x1 - x2 < 0) {
-    _angle += Math.PI;
-  }
-  return _angle;
-};
