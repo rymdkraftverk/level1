7bb9ea67ac4b872a078375ee4bdc911cf917e839
Simon Lagos <simon.lagos.sallhed@gmail.com>
2020-05-10 11:06:53 +0900

    Add performance tests

---
+++ example/performance.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+const { forever, update, get } = require('l1')
+const _ = require('lodash/fp')
+const { performance } = require('perf_hooks')
+
+const _update = () => {
+  _.times(() => forever(() => {}), 1000)
+  const before = performance.now()
+  _.times(update, 120)
+  const after = performance.now()
+  const delta = after - before
+  console.log('Update - Time to run', delta)
+}
+
+const _get = () => {
+  _.times((index) => (forever(() => {}).id = `id-${index}`), 1000)
+  const before = performance.now()
+  _.times((index) => get(`id-${index}`), 1000)
+  const after = performance.now()
+  const delta = after - before
+  console.log('Get - Time to run', delta)
+}
+
+_update()
+_get()
+
+// TODO: Check how many times the functions can be called during 1 sec?
